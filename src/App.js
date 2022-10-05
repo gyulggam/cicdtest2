680606471f3b79b8832e1692a92e34a1850e6c5e
jinsu kim <67sc2@naver.com>
2022-10-05 09:54:23 +0900

    pipeline test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. testtest pipeline 까지 해야하니..?
+          Edit <code>src/App.js</code> and save to reload. testtest pipeline 까지 해야하
         </p>
         <a
           className="App-link"
